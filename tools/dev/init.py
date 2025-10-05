e9376eac02e6bc5c1386a969152e3408d28ba836
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2025-10-05 10:11:45 +0200

    bzl py tool w/ simple pip dep

--- tools/dev/init.py
+++ tools/dev/init.py
@@ -1,1 +1,6 @@
-print('⚙️ dev: init tool')
+from termcolor import * #TODO: use colorama for windows support
+
+print('🚀', colored('Dev: Init tx repo developer environemnt tool', 'light_yellow'))
+
+#TODO: ⚙️ make useful .user.bazelrc
+#TODO: ⚙️ make useful .lldbinit
