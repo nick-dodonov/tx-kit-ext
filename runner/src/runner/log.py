ee23e45685de3339f9fe5cdc72a42bd591815a0f
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2026-02-10 21:53:23 +0100

    Refactor log.py: streamline colorama initialization and enhance Windows stdout/stderr encoding handling

--- runner/src/runner/log.py
+++ runner/src/runner/log.py
@@ -1,16 +1,27 @@
 from typing import Any
+import sys
+
 # https://github.com/tartley/colorama
-from colorama import init, Fore, Back, Style
-#init(autoreset=True) # make termcolor work on windows and simplify usage by auto-resetting styles after each print
-init() # don't auto-reset to allow multi-line styled output
+# from colorama import init
+# init(autoreset=True) # make termcolor work on windows and simplify usage by auto-resetting styles after each print
+# init() # don't auto-reset to allow multi-line styled output
+from colorama import just_fix_windows_console
+just_fix_windows_console() # make termcolor work on windows without auto-resetting styles after each print
+
+# exported for use in other modules
+from colorama import Fore, Style, Back
+
+
+# Fix encoding for piped stdout/stderr on Windows (when used in subprocess.Popen)
+if sys.platform == "win32":
+    try:
+        sys.stdout.reconfigure(encoding='utf-8', errors='replace')
+        sys.stderr.reconfigure(encoding='utf-8', errors='replace')
+    except (AttributeError, OSError):
+        # Python < 3.7 or stream doesn't support reconfigure
+        pass
 
 
 def info(*args: Any, **kwargs: Any) -> None:
     """Print function with automatic flush."""
     print(*args, **kwargs, flush=True)
-
-# print(Fore.RED + 'some red text')
-# print(Back.GREEN + 'and with a green background')
-# print(Style.DIM + 'and in dim text' + Style.RESET_ALL)
-# print(Style.RESET_ALL)
-# print('back to normal now')
