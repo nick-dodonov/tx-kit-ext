e94a8d8db5ce88b18b04e4f4703d91392192adbe
Nick Dodonov <42121323+nick-dodonov@users.noreply.github.com>
2026-03-04 22:03:15 +0100

    Refactor activity lifecycle to run main() onStart and cleanup onStop

--- rules/build/droid/src/cpp/droid_glue.cc
+++ rules/build/droid/src/cpp/droid_glue.cc
@@ -8,8 +8,10 @@
 
 extern int main(int, char**);
 
+static JNIEnv* _env = nullptr;
+
 /// Pass main() result to DroidActivity for System.exit() in onDestroy.
-static void set_exit_code(ANativeActivity* activity, JNIEnv* env, int code)
+static void setExitCode(ANativeActivity* activity, JNIEnv* env, int code)
 {
     jclass clazz = env->GetObjectClass(activity->clazz);
     if (!clazz) return;
@@ -18,23 +20,43 @@ static void set_exit_code(ANativeActivity* activity, JNIEnv* env, int code)
     env->CallVoidMethod(activity->clazz, setExitCode, (jint)code);
 }
 
+static void callMain(ANativeActivity* activity)
+{
+    LOGD("starting main()");
+
+    DroidArgv argv(activity, _env);
+    int result = main(argv.argc(), argv.argv());
+
+    LOGD("finished main(): %d", result);
+    setExitCode(activity, _env, result);
+    ANativeActivity_finish(activity);
+}
+
+static void onStart(ANativeActivity* activity)
+{
+    LOGD("onStart: %p", activity);
+    callMain(activity);
+}
+
+static void onStop(ANativeActivity* activity)
+{
+    LOGD("onStop: %p", activity);
+}
+
+//JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved);
 JNIEXPORT void ANativeActivity_onCreate(ANativeActivity* activity, void* savedState, size_t savedStateSize)
 {
-    LOGD("starting main(): activity=%p savedState=%p savedStateSize=%zu", activity, savedState, savedStateSize);
+    LOGD("onCreate: activity=%p savedState=%p savedStateSize=%zu", activity, savedState, savedStateSize);
+    //JNI_OnLoad(activity->vm, nullptr);
 
-    JNIEnv* env = nullptr;
-    if (activity->vm->AttachCurrentThread(&env, nullptr) != JNI_OK) {
+    if (activity->vm->AttachCurrentThread(&_env, nullptr) != JNI_OK) {
         LOGE("AttachCurrentThread failed");
         ANativeActivity_finish(activity);
         return;
     }
 
+    activity->callbacks->onStart = onStart;
+    activity->callbacks->onStop = onStop;
+
     redirect_stdout_to_logcat();
-
-    DroidArgv argv(activity, env);
-    int result = main(argv.argc(), argv.argv());
-
-    LOGD("finished main(): %d", result);
-    set_exit_code(activity, env, result);
-    ANativeActivity_finish(activity);
 }
